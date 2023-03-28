655dd1032b446fcfb15533be2ae3a79cdd41f3e2
Oliyz <onl2000@hotmail.com>
2023-03-28 10:37:14 +0200

    Så er der update i main 3

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hej med jer! Det er Daniel,Frederik,Nicklas og Oliver jeg håber i har en god dag");
+        System.out.println("så er der update i main 2 igen igen");
     }
 }
